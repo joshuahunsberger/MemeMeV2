77df885461e066233db839518c063f19a8224904
Joshua Hunsberger <joshua@hunsberger.co>
2015-12-06 21:06:56 -0500

    Add custom cell class for table view

---
+++ MemeMeV2/MemeTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MemeTableViewCell.swift
+//  MemeMeV2
+//
+//  Created by Joshua Hunsberger on 12/6/15.
+//  Copyright © 2015 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class MemeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var memeImageView: UIImageView!
+    @IBOutlet weak var topTextLabel: UILabel!
+    @IBOutlet weak var bottomTextLabel: UILabel!
+}
