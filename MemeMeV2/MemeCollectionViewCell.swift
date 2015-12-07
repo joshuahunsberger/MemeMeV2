428400bce2f5b3d136aedc7dc4d1137f8cbae0cb
Joshua Hunsberger <joshua@hunsberger.co>
2015-12-06 20:58:22 -0500

    Add MemeCollectionView and MemeCollectionViewCell and add references to storyboard

---
+++ MemeMeV2/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMeV2
+//
+//  Created by Joshua Hunsberger on 12/6/15.
+//  Copyright © 2015 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var memeImageView: UIImageView!
+}
---
+++ MemeMeV2/MemeCollectionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MemeCollectionViewController.swift
+//  MemeMeV2
+//
+//  Created by Joshua Hunsberger on 11/29/15.
+//  Copyright © 2015 Joshua Hunsberger. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewController: UICollectionViewController {
+    var memes : [Meme] {
+        return (UIApplication.sharedApplication().delegate as! AppDelegate).memes
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MemeCollectionViewCell", forIndexPath: indexPath) as! MemeCollectionViewCell
+
+        cell.memeImageView.image = memes[indexPath.row].memeImage
+        
+        return cell
+    }
+}
